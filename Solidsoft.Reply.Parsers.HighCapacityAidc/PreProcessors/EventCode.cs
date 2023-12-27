2f321d64655e7eb16533a77c9165599e2ee1f62e
ch.young <ch.young@reply.eu>
2023-12-27 14:04:20 +0000

    Updated DOM3 keyboard event code pre-processor

---
+++ Solidsoft.Reply.Parsers.HighCapacityAidc/PreProcessors/EventCode.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Solidsoft.Reply.Parsers.HighCapacityAidc.PreProcessors;
+
+/// <summary>
+///   Represents a DOM 3 event code.
+/// </summary>
+public class EventCode {
+
+    /// <summary>
+    /// The event code.
+    /// </summary>
+    [JsonProperty("code", Order = 0)]
+    public string Code { get; set; }
+
+    /// <summary>
+    /// Key modifiers.
+    /// </summary>
+    [JsonProperty("modifiers", Order = 1)]
+    public int Modifiers { get; set; }
+}
